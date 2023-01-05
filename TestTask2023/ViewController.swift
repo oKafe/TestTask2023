cea1fcc5a95361641fd0052275d718d096b533c3
andrii_syrota42 <andrii.s.ios@doneit.net>
2023-01-05 14:20:29 +0200

    Initial Commit

---
+++ TestTask2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask2023
+//
+//  Created by JkPhTrue Just on 05.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
